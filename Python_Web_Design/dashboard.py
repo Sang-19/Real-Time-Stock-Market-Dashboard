fdeee5eedc0506107900e22e92f2c5653aea4316
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-02-02 22:55:31 +0530

    Update dashboard.py

--- Python_Web_Design/dashboard.py
+++ Python_Web_Design/dashboard.py
@@ -1,13 +1,32 @@
 import streamlit as st 
 import yfinance as yf
+import plotly.express as pe
+
+
 
 st.title("Stock Dashboard")
 st.sidebar.title("Details Required")
-ticket_symbol = st.sidebar.text_input("Enter Stock Name","MSFT")
+ticket_symbol = st.sidebar.text_input("Enter Stock Name","")
 start_date = st.sidebar.date_input("Start Date:",value=None)
 end_date = st.sidebar.date_input("End Date:",value=None)
 
 ticket = yf.Ticker(ticket_symbol)
-st.write("Hello")
+#st.write("Stock Market Data")
 historical_data = ticket.history(start = start_date, end = end_date)
-st.write(historical_data)
+if start_date is not None and end_date is not None:
+  #t.write(historical_data)
+  st.subheader(f'{ticket_symbol} Stock View')
+  stockData = yf.download(ticket_symbol,start = start_date, end = end_date)
+  Price, Historical, Chart = st.tabs(["Price Summary", "Historical Summary", "Charts"])
+  with Price:
+    st.write("Price Summary")
+    st.write(stockData)
+  with Historical:
+    st.write("Historical Data")
+    st.write(historical_data)
+  with Chart:
+    st.write("Charts")
+    stockData.columns = stockData.columns.get_level_values(0)
+    line_charts = pe.line(stockData, stockData.index, y="Close", title=ticket_symbol)
+    st.plotly_chart(line_charts)
+  
