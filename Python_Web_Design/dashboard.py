f9cebfbb13fe43face3fba98405827c3407366d3
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-02-02 22:56:04 +0530

    Update dashboard.py

--- Python_Web_Design/dashboard.py
+++ Python_Web_Design/dashboard.py
@@ -1,15 +1,11 @@
 import streamlit as st 
 import yfinance as yf
 import plotly.express as pe
-
-
-
 st.title("Stock Dashboard")
 st.sidebar.title("Details Required")
 ticket_symbol = st.sidebar.text_input("Enter Stock Name","")
 start_date = st.sidebar.date_input("Start Date:",value=None)
 end_date = st.sidebar.date_input("End Date:",value=None)
-
 ticket = yf.Ticker(ticket_symbol)
 historical_data = ticket.history(start = start_date, end = end_date)
 if start_date is not None and end_date is not None:
